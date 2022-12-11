1c9c23b944911c73858fc3c8ff34f1c141c84a29
AdamJPHunt <u2186477@live.warwick.ac.uk>
2022-12-11 10:34:11 +0000

    Initial Import

---
+++ domain_search_test.py
@@ -0,0 +1,30 @@
+from contextlib import redirect_stdout
+from Bio import SeqIO
+from pyhmmer import hmmsearch
+from pyhmmer import hmmpress
+from pyhmmer import hmmer
+from pyhmmer import phmmer
+import subprocess
+import pyhmmer
+from pyhmmer import
+
+
+fasta_sequences = SeqIO.parse(open("caelestamide.fasta"),'fasta')
+with open('output.txt', 'w') as out_file:
+    for fasta in fasta_sequences:
+        #x = '1'
+        #with redirect_stdout('output.txt'):
+        #out_file.write(x)
+        #name, sequence = fasta.id, str(fasta.seq)
+        #new_sequence = hmmscan(sequence, test) #run hmmscan here
+        #write_fasta('query.fasta')
+
+hmmbuild thioesterase.hmm thioesterase.sto
+hmmbuild p450.hmm p450.sto
+hmmbuild condensation.hmm condensation.sto
+cat condensation.hmm p450.hmm thioesterase.hmm > test
+hmmpress test
+hmmscan test test.fasta
+hmmscan -o 'output.txt' --noali  test test.fasta
+
+
